d1f9d73dfd4ef7ab3c802454959bc3f638dec914
Irem Kenar <irem.kenar@outlook.com>
2021-03-07 19:47:57 +0300

    Added method 3 for the solution

--- app.js
+++ app.js
@@ -46,6 +46,31 @@ function removeTransition(e) {
 window.addEventListener("keydown", playSound);************/
 
 
+/************ Method 3
+function initiateEventListeners() {
+    window.addEventListener("keydown", playSound);
+    const keys = document.querySelectorAll(".key");
+    keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+}
+
+function playSound(e) {
+    const pressKey = document.querySelectorAll(`[data-key="${e.keyCode}"]`);
+    const pressedKey = pressKey.item(0);
+    const pressedKeyAudio = pressKey.item(1);
+    if(!pressedKeyAudio) return; // stop the function from running altogether
+    pressedKeyAudio.play();
+    pressedKeyAudio.currentTime = 0; //rewind to the start
+    pressedKey.classList.add("playing");
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return; // skip if it's not a transform
+    this.classList.remove("playing");
+}
+
+initiateEventListeners(); *****************/
+
+
 
 
 
